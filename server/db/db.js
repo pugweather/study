84dcb20dcc8397c7b857d2625687b2d1982e9a70
myles <myles.holt.12@gmail.com>
2026-03-22 21:23:50 -0400

    -add db connection

---
+++ server/db/db.js
@@ -0,0 +1,13 @@
+import sqlite3 from 'sqlite'
+import { open } from 'sqlite3'
+import path from 'node:path'
+
+export async function getDBConnection() {
+
+    const dbPath = path.join('database.db')
+
+    return open({
+        filename: dbPath,
+        driver: sqlite3.Database
+    })
+}
